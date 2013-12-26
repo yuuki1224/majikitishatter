2fe041e1f5e385e9ce0d0cd848d9a293a024f261
Asano Yuuki <yuuki.1224.softtennis@gmail.com>
2013-12-26 13:55:24 +0900

    [Add]ViewControllers

---
+++ majikichishatter/majikichishatter/SettingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingViewController.h
+//  majikichishatter
+//
+//  Created by 浅野 友希 on 2013/12/26.
+//  Copyright (c) 2013年 Kazuki Taniguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingViewController : UIViewController
+
+@end
