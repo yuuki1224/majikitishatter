c14646be8cec119bfb537bd9eb016bda00102e50
Kazuki <kazuki.t.1018@gmail.com>
2013-12-26 12:49:38 +0900

    initial commit

---
+++ majikichishatter/majikichishatter/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  majikichishatter
+//
+//  Created by Kazuki Taniguchi on 2013/12/26.
+//  Copyright (c) 2013年 Kazuki Taniguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ majikichishatter/majikichishatter/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  majikichishatter
+//
+//  Created by Kazuki Taniguchi on 2013/12/26.
+//  Copyright (c) 2013年 Kazuki Taniguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
